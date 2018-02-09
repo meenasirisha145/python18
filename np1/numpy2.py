06b5ac81b3904cedd280fec52b8d2e62681050b9
meena sirisha145 <meenasirisha145@gmail.com>
2018-02-09 14:52:22 +0530

    02/08/2018

--- np1/numpy2.py
+++ np1/numpy2.py
@@ -8,6 +8,7 @@ a=np.array([[0,1,2,3],[10,11,12,13]])
 a
 a.shape
 np.shape(a)
+#%%numpy arrays
 
 type(a)
 a.size
@@ -29,13 +30,21 @@ b=a1[:,::2].copy()
 b[0,1]=99
 b
 a1
-
+#%%fancy indexing
 a=arange(0,80,10)
 a
 y=a[[1,2,-3]]
 y
 z=take(a,[1,2,-3])
 z
+type([a[1],a[2],a[-3]])
+type(z)
+
+ind=[1,2,-3]
+x=take(a,ind)
+x
+
+#%%masking
 mask=array([0,1,1,0,1,1,0,0],dtype=bool)
 mask1=array([True,False,True,False,True,False,False,False],dtype=bool)
 x=a[mask]
@@ -53,28 +62,29 @@ x
 mask=array([1,0,1,0,0,1],dtype=bool)
 x[mask,2]
 
-
+#%%array caluculation methods
 a1
 a1[0,2]=3
 a1
 sum(a)
-sum(a1)
+%timeit sum(a1)
+%timeit a1.sum()
 sum(a1,axis=0)
 sum(a1,axis=-1)
-prod(a1,axis=0)#columnwise
-prod(a1,axis=1)#rowwise
+prod(a1,axis=0)#columnwise for axis=0
+prod(a1,axis=1)#rowwise for axis=1
 a1.min(axis=0)
 a1.min(axis=1)
 amin(a1,axis=0)
 argmin(a1,axis=0)
 argmin(a1,axis=1)
-a1.max(axis=0)
+a1.max(axis=None)
 a1.max(axis=1)
 a1.argmax(axis=0)
 a1.argmax(axis=1)
 a1.mean(axis=0)
 a1.mean(axis=1)
-average(a1,axis=0)
+average(a1,axis=None)
 average(a1,weights=[1,2],axis=0)
 a1.std(axis=0)
 a1.clip(3,5)
@@ -82,7 +92,7 @@ a1
 a1.ptp(axis=0)#max-min=range
 a1.ptp(axis=None)#range for entire array
 
-
+#%%Comparison and Logical Operators
 a=array([1,2,3,4])
 b=array([4,5,6,7])
 a+b
@@ -90,3 +100,37 @@ a==b
 a<=b
 a>=b
 a!=b
+
+
+a1=array([[1,2,3],[4,5,6],[7,8,9]],float)
+a1
+average(a1,weights=[1,2,3],axis=0)
+
+
+#%%Shape Operations
+a1.flatten()#converts multi-dimensional array to 1D array
+a1
+a1.ravel()#same as flatten but returns a view if possible
+id(a1)
+b=a1
+id(b)
+c=a1.copy()
+id(c)
+
+a1.swapaxes(0,1)#transpose
+a1
+a.resize(4,2)
+a
+np.array([1,2,3,4,5,6]).reshape(3,2)
+np.array([1,2,3,4,5,6]).reshape(6,1).squeeze()
+np.array([1,2,3,4,5,6]).reshape(1,6).squeeze()
+
+a.T ##transpose
+a.squeeze()#transpose
+a
+a.T
+a[2,0]=0
+a
+a.nonzero()
+a.cumsum(axis=None)
+a1.cumprod(axis=None)
